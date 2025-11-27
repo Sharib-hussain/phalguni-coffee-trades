3bc4ee3d5a8788fe1e39814989d41138a024c8da
sharib-hussain <mohammedsharib289@gmail.com>
2025-11-27 23:21:47 +0530

    Add GitHub Actions workflow for automatic deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/phalguni-coffee-trades/',
   build: {
     outDir: 'docs'
   }
